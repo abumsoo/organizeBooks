317c68a3385e3a17527e59004714820e03ce6b74
Bum Kim <bumcrystlbum@gmail.com>
2019-05-22 12:17:42 -0600

    got deletion to start working, but title matching is too flimsy

--- organize.py
+++ organize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import csv
 import json
 import requests
 
@@ -11,12 +12,12 @@ def searchBook(title):
     data = response.json()
     docs = data["docs"]
 
-    print(json.dumps(data, indent=4, sort_keys=True))
+    # print(json.dumps(data, indent=4, sort_keys=True))
     latestPub = ''
     for doc in docs:
         # check title matches and there exists a publish year
         titleCurrent = doc["title"].lower()
-        if "publish_year" in doc and title.lower() in titleCurrent:
+        if "publish_year" in doc and title.lower() == titleCurrent:
             if latestPub == '':
                 latestPub = doc
             elif latestPub["publish_year"] < doc["publish_year"]:
@@ -34,9 +35,15 @@ def enterBook():
         title = input("Enter the title of the book: ")
         f.write(title + '\n')
 
-def delBook():
+def delBook(title):
+    current = []
+    with open('books') as f:
+        for book in f:
+            if title not in book:
+                current.append([book])
     with open('books', 'w') as f:
-        title = input("Enter the title of the book: ")
+        writer = csv.writer(f)
+        writer.writerows(current)
 
 def readList():
     with open('books') as f:
@@ -46,20 +53,24 @@ def readList():
 def main():
     choice = None
     while choice != 'q':
-        print("(e)nter a book")
-        print("show book (l)ist")
+        print("(e)nter a book into your list")
+        print("(p)rint your book list")
         print("(s)earch for a title")
+        print("(d)elete a title from your list")
         print("(q)uit")
         choice = input()
         if choice == 'q':
             break
         elif choice == 'e':
             enterBook()
-        elif choice == 'l':
+        elif choice == 'p':
             readList()
         elif choice == 's':
             title = input("Enter a title: ")
             searchBook(title)
+        elif choice == 'd':
+            title = input("Enter a title: ")
+            delBook(title)
         else:
             print("Not a valid option.")
 
